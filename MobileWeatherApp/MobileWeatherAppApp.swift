b530938f7feb933b58cbe9567aff133410ef956b
khawlah-kh <44741544+khawlah-kh@users.noreply.github.com>
2021-12-31 10:56:51 +0300

    Initial Commit

---
+++ MobileWeatherApp/MobileWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileWeatherAppApp.swift
+//  MobileWeatherApp
+//
+//  Created by khawlah khalid on 31/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct MobileWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
